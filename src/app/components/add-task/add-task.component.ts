469d81c9cf2917ddd92a38763e7b633d0083b600
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-08-13 07:39:53 +0200

    about

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-add-task',
@@ -12,9 +15,12 @@ export class AddTaskComponent implements OnInit {
   text: string = '';
   day: string = '';
   reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription = new Subscription();
   
 
-  constructor() {    
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(i => this.showAddTask = i)
    }
 
   ngOnInit(): void {
@@ -22,22 +28,28 @@ export class AddTaskComponent implements OnInit {
 
   onSubmit() {
     if (!this.text || !this.day) {
-      alert('Please add a text, day & time')
-    }
+      alert('Please add a text, day & time');
+      return;
+    } 
     
     const newTask = {
       text: this.text,
       day: this.day,
       reminder: this.reminder
-    }
+      }
+      
+      // @todo - emit event
+      this.onAddTask.emit(newTask)
 
-    // @todo - emit event
-    this.onAddTask.emit(newTask)
+  
 
     this.text = '';
     this.day = '';
     this.reminder = false;
+    
 
   }
+
+
 
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -6,14 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title:string = 'Task Tracker';
-  constructor() { }
+  title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription = new Subscription();
+  
+  constructor(private uiService: UiService, private router:Router) {
+    this.subscription = this.uiService.onToggle().subscribe(i => this.showAddTask = i)
+   }
 
   ngOnInit(): void {
   }
 
   toggleAddText() {
-    console.log('toggle')
+    this.uiService.toggleAddTask()    
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 
 }
